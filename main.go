e4dde2de506768bca7e4704d2c390ef7803499a2
zeristor <geoff.hogg@gmail.com>
2018-03-19 00:32:18 +0000

    Basic website up and running

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint hit")
+}
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	handleRequests()
 }
